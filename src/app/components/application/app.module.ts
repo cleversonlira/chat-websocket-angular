c334c7f043108129ba5f979e763b9d68401079f9
cleversonlira <cleverson_lira@outlook.com>
2022-12-04 15:02:56 -0300

    Estrutura do projeto

--- src/app/components/application/app.module.ts
+++ src/app/components/application/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ChatComponent } from '../chat/chat.component';
+import {FormsModule} from "@angular/forms";
+import {AppComponent} from "./app.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
